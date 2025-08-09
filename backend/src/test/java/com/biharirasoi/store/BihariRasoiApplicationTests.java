3fd913da9d9ae5bd9184d27dbfc12bd53c5d7e91
shikha1304 <shikha04sinha@gmail.com>
2025-08-09 20:09:13 +0530

    Initial backend configurations added

---
+++ backend/src/test/java/com/biharirasoi/store/BihariRasoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biharirasoi.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BihariRasoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
